1054e136b2364532bfec70b646482feb28316f3f
Federico Pedraza <federico.pedraza@nubiral.com>
2025-06-07 06:58:41 -0300

    tried the calendar on a different approach

---
+++ src/components/calendar/CalendarHour.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  containerClass?: string,
+  hour: number
+}>()
+</script>
+
+<template>
+    <div class="w-4 flex items-start justify-start text-xs text-gray-500" :class="containerClass">
+      <span class="hour" v-if="hour !== 0">{{ hour === 0 ? 12 : hour > 12 ? hour - 12 : hour }} {{ hour < 12 ? 'AM' : 'PM' }}</span>
+    </div>
+</template>
+
+<style scoped lang="css">
+  .hour {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: 0%;
+    color: #71717A;
+  }
+</style>
--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import CalendarHours from '../components/calendar/CalendarHours.vue'
+import CalendarHour from '../components/calendar/CalendarHour.vue'
 import CalendarCell from '../components/calendar/CalendarCell.vue'
 import CalendarEvent from '../components/calendar/CalendarEvent.vue'
 
@@ -17,22 +17,16 @@ const isToday = (day: string) => day === 'MON'
 </script>
 
 <template>
-  <!-- CALENDAR TABLE -->
-  <div class="flex flex-row justify-between w-full p-4 gap-4">
-    <!-- SIDE LEFT HOURS-->
-    <div class="flex flex-col">
-      <!-- DAY HEADER SPACER -->
-      <div class="min-w-36 min-h-16 bg-red-500"></div>
-      <!-- HOURS -->
-      <CalendarHours hourClass="justify-end" />
-    </div>
+  <div class="flex flex-col w-full py-4 px-4">
+    <!-- HEADER -->
+    <div class="flex flex-row w-full">
+      <!-- LEFT HOUR SPACER -->
+      <div class="min-w-10 min-h-16"></div>
 
-    <!-- CALENDAR -->
-    <div class="">
-      <!-- DAYS HEADER -->
-      <div class="flex flex-row">
-        <div class="flex flex-col" v-for="(day, index) in days" :key="day">
-          <CalendarCell :class="[
+      <!-- DAY HEADERS -->
+      <div class="flex flex-grow w-full">
+        <div class="flex flex-grow" v-for="(day, index) in days" :key="day">
+          <CalendarCell class="w-full" :class="[
             'pt-1 pr-2 pb-4 pl-2 border-[#E0E0E0] border-[1px] border-t-0',
             isToday(day) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
             index === 0 ? 'border-l-0' : '',
@@ -43,26 +37,32 @@ const isToday = (day: string) => day === 'MON'
               <span class="whitespace-nowrap text-2xl text-[#000000] font-bold">21</span>
             </div>
           </CalendarCell>
-          <!-- HOURS ON DAY -->
-          <div class="flex flex-col">
-            <CalendarCell :class="[
-            'pt-1 pr-2 pb-4 pl-2 border-[#E0E0E0] border-[1px] border-t-0',
+        </div>
+      </div>
+
+      <!-- RIGHT HOUR SPACER -->
+      <div class="min-w-10 min-h-16"></div>
+    </div>
+
+    <!-- BODY -->
+    <div v-for="hour in hours" :key="hour" class="flex flex-row min-h-16 w-full">
+      <!-- LEFT HOUR LABEL -->
+      <CalendarHour class="min-w-10 pr-4 whitespace-nowrap" :hour="hour" containerClass="justify-end" />
+
+      <!-- MAIN CALENDAR CELLS -->
+      <div class="flex flex-grow w-full">
+        <div class="flex flex-grow" v-for="(day, index) in days" :key="day + '-' + hour">
+          <CalendarCell class="w-full h-full" :class="[
+            'pt-1 pr-2 pb-4 pl-2 border-[#E0E0E0] border-[1px]',
             isToday(day) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
             index === 0 ? 'border-l-0' : '',
             index === days.length - 1 ? 'border-r-0' : '',
-          ]" v-for="hour in hours" :key="hour">
-            <span>{{ hour }}</span>
-</CalendarCell>
-          </div>
+          ]" />
         </div>
       </div>
-    </div>
-    <!-- SIDE RIGHT HOURS -->
-    <div class="flex flex-col">
-      <!-- DAY HEADER SPACER -->
-      <div class="min-w-36 min-h-16 bg-red-500"></div>
-      <!-- HOURS -->
-      <CalendarHours />
+
+      <!-- RIGHT HOUR LABEL -->
+      <CalendarHour class="min-w-10 pl-4 whitespace-nowrap" :hour="hour" containerClass="justify-start" />
     </div>
   </div>
 </template>
