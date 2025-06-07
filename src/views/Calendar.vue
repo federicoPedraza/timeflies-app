ee83bfd1087e8770ff54e1ad256631b7a79cd4ad
Federico Pedraza <federico.pedraza@nubiral.com>
2025-06-07 07:22:14 -0300

    added actual dates

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import { ref, onMounted, nextTick } from 'vue'
+import { startOfWeek, addDays, isSameDay } from 'date-fns'
 import CalendarHour from '../components/calendar/CalendarHour.vue'
 import CalendarCell from '../components/calendar/CalendarCell.vue'
-import CalendarEvent from '../components/calendar/CalendarEvent.vue'
+import { formatDate } from '@/utils/dates/date-formatter'
 
 const startsWithSunday = true
 
@@ -11,10 +12,15 @@ const days = startsWithSunday
   ? [...baseDays.slice(-1), ...baseDays.slice(0, -1)]
   : baseDays
 
+const today = new Date()
+const weekStart = startOfWeek(today, { weekStartsOn: startsWithSunday ? 0 : 1 })
+
+const dateObjects = days.map((_, index) => addDays(weekStart, index))
+
 const hours = Array.from({ length: 24 }, (_, i) => i)
 
 const isWeekend = (day: string) => day === 'SAT' || day === 'SUN'
-const isToday = (day: string) => day === 'MON'
+const isToday = (date: Date) => isSameDay(date, today)
 
 const bodyScrollContainer = ref<HTMLElement | null>(null)
 
@@ -42,12 +48,14 @@ onMounted(async () => {
         <div class="flex flex-grow" v-for="(day, index) in days" :key="day">
           <CalendarCell class="w-full" :class="[
             'pt-1 pr-2 pb-4 pl-2 border-[#E0E0E0]',
-            isToday(day) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
+            isToday(dateObjects[index]) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
             index === 0 ? 'border-l-[0px]' : 'border-l-[1px]',
           ]">
             <div class="flex flex-col">
               <span class="whitespace-nowrap font-bold text-[10px] text-[#71717A]">{{ day }}</span>
-              <span class="whitespace-nowrap text-2xl text-[#000000] font-bold">21</span>
+              <span class="whitespace-nowrap text-2xl text-[#000000] font-bold">
+                {{ formatDate(dateObjects[index], 'dd') }}
+              </span>
             </div>
           </CalendarCell>
         </div>
@@ -68,7 +76,7 @@ onMounted(async () => {
           <div class="flex flex-grow" v-for="(day, index) in days" :key="day + '-' + hour">
             <CalendarCell class="w-full h-full" :class="[
               'pt-1 pr-2 pb-4 pl-2 border-[#E0E0E0] border-[1px] border-b-0 border-r-0',
-              isToday(day) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
+              isToday(dateObjects[index]) ? 'bg-[#EFF6FF]' : isWeekend(day) ? 'bg-[#FAFAFA]' : 'bg-white',
               index === 0 ? 'border-l-0' : '',
             ]" />
           </div>
